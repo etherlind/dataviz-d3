ac2d2a0c26701f7d6e123be15849bf4296b0c888
camila <camila.yusupova@skitudeservices.com>
2020-04-07 14:03:05 +0200

    initial add

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
-// DOM elements
-const btns = document.querySelectorAll('button');
-const form = document.querySelector('form');
-const formAct = document.querySelector('form span');
-const input = document.querySelector('input');
-const error = document.querySelector('.error');
+const data = [
+    { name: 'news', parent: '' },
 
-let activity = 'cycling';
+    { name: 'tech', parent: 'news' },
+    { name: 'sport', parent: 'news' },
+    { name: 'music', parent: 'news' },
 
-btns.forEach(btn => {
+    { name: 'ai', parent: 'tech', amount: 7 },
+    { name: 'coding', parent: 'tech', amount: 5 },
+    { name: 'tablets', parent: 'tech', amount: 4 },
+    { name: 'laptops', parent: 'tech', amount: 6 },
+    { name: 'd3', parent: 'tech', amount: 3 },
+    { name: 'gaming', parent: 'tech', amount: 3 },
 
-    btn.addEventListener('click', e => {
-        // get activity
-        activity = e.target.dataset.activity;
+    { name: 'football', parent: 'sport', amount: 6 },
+    { name: 'hockey', parent: 'sport', amount: 3 },
+    { name: 'baseball', parent: 'sport', amount: 5 },
+    { name: 'tennis', parent: 'sport', amount: 6 },
+    { name: 'f1', parent: 'sport', amount: 1 },
 
-        // remove and add active class
-        btns.forEach(btn => btn.classList.remove('active'));
-        e.target.classList.add('active');
+    { name: 'house', parent: 'music', amount: 3 },
+    { name: 'rock', parent: 'music', amount: 2 },
+    { name: 'punk', parent: 'music', amount: 5 },
+    { name: 'jazz', parent: 'music', amount: 2 },
+    { name: 'pop', parent: 'music', amount: 3 },
+    { name: 'classical', parent: 'music', amount: 5 },
+];
 
-        // set id of input field
-        input.setAttribute('id', activity);
+const stratify = d3.stratify()
+    .id(d => d.name)
+    .parentId(d => d.parent)
 
-        formAct.textContent = activity;
-
-        update(data);
-    });
-});
-
-// form submit
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    const distance = parseInt(input.value);
-    if (distance) {
-        db.collection('activities').add({
-            distance,
-            activity,
-            date: new Date().toString()
-        }).then(() => {
-            error.textContent = '';
-            input.value = '';
-        });
-    } else {
-        error.textContent = 'Please enter a valid distance';
-    }
-});
+console.log(stratify(data));
