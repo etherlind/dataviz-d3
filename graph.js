e2fb4930b7f94475cf10fa4eb440e18e3a85afc1
camila <camila.yusupova@skitudeservices.com>
2020-04-15 09:40:11 +0200

    added the nodes and the links

--- graph.js
+++ graph.js
@@ -1,6 +1,6 @@
 const dims = { height: 500, width: 1100 };
 
-const svg = d3.select('canvas')
+const svg = d3.select('.canvas')
     .append('svg')
     .attr('width', dims.width + 100)
     .attr('height', dims.height + 100);
@@ -16,24 +16,68 @@ const stratify = d3.stratify()
 const tree = d3.tree()
     .size([dims.width, dims.height]);
 
+const color = d3.scaleOrdinal(['#b3cde3', '#8c96c6', '#8856a7', '#810f7c']);
+
 // update function
 const update = data => {
+
+    // remove current nodes
+
+    graph.selectAll('.node').remove();
+    graph.selectAll('.link').remove();
+
+    // update ordinal scale domain
+    color.domain(data.map(item => item.department));
 
     // get updated root Node data
     const rootNode = stratify(data);
     const treeData = tree(rootNode);
 
     // get nodes selection and join data
-    const nodes = graph.selectAll('.node')
+    const nodes = graph
+        .selectAll('.node')
         .data(treeData.descendants());
 
+    // get link selection and join data
+    const links = graph.selectAll('.link')
+        .data(treeData.links());
+    console.log(treeData.links());
+
+    // enter new links
+    links.enter()
+        .append('path')
+        .attr('class', 'link')
+        .attr('fill', 'none')
+        .attr('stroke', '#aaa')
+        .attr('stroke-width', 2)
+        .attr('d', d3.linkVertical()
+            .x(d => d.x)
+            .y(d => d.y))
+
     // create enter node groups
-    const enterNodes = nodes.enter()
+    const enterNodes = nodes
+        .enter()
         .append('g')
         .attr('class', 'node')
         .attr('transform', d => `translate(${d.x},${d.y})`);
 
-    console.log(treeData);
+    enterNodes
+        .append('rect')
+        .attr('fill', d => color(d.data.department))
+        .attr('stroke', '#555')
+        .attr('stroke-width', 2)
+        .attr('height', 50)
+        .attr('width', d => d.data.name.length * 20)
+        .attr('transform', d => {
+            const x = d.data.name.length * 20 / 2;
+            return `translate(${-x}, -32)`;
+        });
+
+    enterNodes
+        .append('text')
+        .attr('text-anchor', 'middle')
+        .attr('fill', 'white')
+        .text(d => d.data.name);
 };
 
 // data & firebase hook-up
